5eacaaf0678f15fe74e97d6834c147a00fb124c9
Riley Hoock <laurenh8916@protonmail.com>
2022-12-16 09:51:35 -0700

    added weapon mods to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,18 @@ require 'json'
 def createItems(version)
   files = Dir['./lib/tasks/scraper/*.json']
   files.delete('./lib/tasks/scraper/attributes.json')
-  files.delete('./lib/tasks/scraper/weapon_mods.json')
 
   files.each do |file|
     json_array = JSON.parse(File.read(file))
 
     for hash in json_array do
-      # Create item 
-      new_item = version.weapons.new()
+      if file == './lib/tasks/scraper/weapons.json'
+        new_item = version.weapons.new()
+      elsif file == './lib/tasks/scraper/weapon_mods.json'
+        new_item = version.weapon_mods.new()
+      else
+        puts 'Error!'
+      end
       
       hash.each do |k,v|
         new_item[k.to_sym] = v 
@@ -18,21 +22,6 @@ def createItems(version)
 
       new_item.save 
     end
-
-    # Create item
-    # case file
-    # when './lib/tasks/scraper/weapons.json'
-      
-    #   json.each do |item_hash|
-    #     item = version.weapons.create()
-    #     item_hash.each_key do |k,v|
-           
-          
-    #     end
-    #   end
-    # else
-    #   puts 'oops'
-    # end
     
   end
 end
